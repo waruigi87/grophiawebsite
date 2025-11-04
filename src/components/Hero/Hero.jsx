b10429df6a8c538e4d525578a274a058a803c501
Akifumi <waruigi87@gmail.com>
2025-11-04 20:25:21 +0900

    Heroページのソース画像をpublicフォルダに移動しました

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
             initial={{ x: -100, opacity: 0 }}
             animate={{ x:0 , opacity: 1 }}
             transition={{ duration: 0.8, delay: 1 }}
-         className='bg-cover bg-center h-screen relative' style={{backgroundImage: "url('/src/assets/Hero/ホームページ背景画像_Hero1.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', height: '80vh'}}>
+         className='bg-cover bg-center h-screen relative' style={{backgroundImage: "url('/ホームページ背景画像_Hero1.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', height: '80vh'}}>
            <div className='absolute inset-0 bg-black bg-opacity-60 z-0'></div>
             <div className='relative z-10 items-center justify-center h-full flex flex-col gap-6'>
                 <div className="container">
